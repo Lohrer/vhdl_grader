7fc9b2dace74af5a8dd5d68371350764cafb4740
Michael Lohrer <michael.f.lohrer@gmail.com>
2018-02-01 03:51:30 -0500

    Added student comparison

--- comparelabs.py
+++ comparelabs.py
@@ -11,6 +11,20 @@
 
 import sys, os, shutil, re
 from pyunpack import Archive
+from difflib import SequenceMatcher
+
+def get_file_text(filename):
+    try:
+        f = open(filename)
+    except OSError as err:
+        return ''
+    else:
+        try:
+            return f.read()
+        except:
+            return ''
+        finally:
+            f.close()
 
 # Supported archive formats for extracting
 archive_extensions = ('.zip', '.rar', '.7z')
@@ -68,23 +82,58 @@ for dir in os.listdir('.'):
             for dirpath, dirnames, filenames in os.walk(path, topdown=False):
                 for file in filenames:
                     if file.lower().endswith(('.vhd', '.xdc')):
-                        os.rename(os.path.join(dirpath, file), os.path.join(name, file))
+                        os.rename(os.path.join(dirpath, file),
+                            os.path.join(name, file))
                     else:
                         os.remove(os.path.join(dirpath, file))
                 for d in dirnames:
                     os.rmdir(os.path.join(dirpath, d))
-            os.rmdir(path)
+            os.rmdir(path) # TODO: Remove any leftover non-.vhd/.xdc files
 
-# Create a list of students
-names = os.listdir('.')
-#for name in names:
-#    print(name)
+# Check for .xdc files and remove them
+names = os.listdir('.') # Create a list of students
+for name in names:
+    files = os.listdir(name)
+    xdc_files = [xdc for xdc in files if xdc.endswith('.xdc')]
+    if len(xdc_files) > 1:
+        print(name + ' has multiple XDC files.')
+    elif not xdc_files:
+        print(name + ' has no XDC file.')
+    for file in xdc_files:
+        os.remove(os.path.join(name, file))
 
-# Verify existence of testbenches and constraints files
+# Compare all students for similarity
+# TODO: cut time in half by only comparing each pair once
+for name1 in names:
+    # Get all of name1's files
+    files1 = os.listdir(name1)
+    for name2 in names:
+        if name2 == name1:
+            continue
 
+        # Get all of name2's files
+        files2 = os.listdir(name2)
 
-#re.search('entity .* is.*\nend .*;', text, flags=re.IGNORECASE)
+        # Calculate the average similarity over each of name1's files
+        similarities = []
+        for file1 in files1:
+            file1_text = get_file_text(os.path.join(name1, file1))
+            # Find the file that most similary matches file1
+            max_similarity = 0.0;
+            most_similar_file = ''
+            for file2 in files2:
+                file2_text = get_file_text(os.path.join(name2, file2))
+                sim12 = SequenceMatcher(None, file1_text, file2_text).ratio()
+                if sim12 > max_similarity:
+                    max_similarity = sim12
+                    most_similar_file = file2
+            #if max_similarity > 0.95:
+            #    print(file1 + ' is similar to ' + most_similar_file +
+            #        ' to degree ' + str(max_similarity))
+            similarities.append(max_similarity)
+        mean = sum(similarities) / float(len(similarities))
+        if mean > 0.9:
+            print('Student ' + name1 + ' and ' + name2 +
+                ' are similar to degree ' + str(mean))
 
-
-# Compare each student's files with each other's.
-
+# TODO: Verify existence of testbenches
